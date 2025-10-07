ee86cd81ec8c3b1f377964257d4af2bfa2fd1e1f
Drew Eckhart <andrew.eckhart6@gmail.com>
2025-10-07 09:49:02 -0400

    updates

--- python_embedding_api/__init__.py
+++ python_embedding_api/__init__.py
@@ -2,7 +2,7 @@ from google.cloud import aiplatform
 from google.oauth2 import service_account
 import vertexai
 
-my_credentials = service_account.Credentials.from_service_account_file("unvailed-service-account.json")
+my_credentials = service_account.Credentials.from_service_account_file("../unvailed_service_account_key.json")
 PROJECT_ID = "unvailed-466101"
 LOCATION = "us-central1"
 BUCKET_NAME = "unvailed_test_bucket_1"
--- python_embedding_api/app.py
+++ python_embedding_api/app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import uvicorn
+
 
 app = FastAPI(title="My API", version="1.0.0")
-
 
 # include routers
 # app.include_router(items.router)
@@ -10,5 +11,14 @@ app = FastAPI(title="My API", version="1.0.0")
 def root():
     return {"message": "Welcome to My API!"}
 
-if __name__ == 'main':
-    app.run(host='0.0.0.0', port=8080)
+@app.post("/transformAndEmbedData")
+async def transformAndEmbedData(request: Request):
+    data = await request.json()
+    return {
+        "message": "The api has received the data!",
+        "data": data
+        }
+
+
+if __name__ == '__main__':
+    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
